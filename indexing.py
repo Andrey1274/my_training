642e13a25c6274ffa5ca932ee89c7a35020bbad6
Andrey1274 <berdin1274@gmail.com>
2024-10-14 00:14:19 +0500

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,10 @@
+example = 'Andrey Berdin'
+print(example)
+print(example[0])
+print(example[-1])
+quanti = (len(example))
+print(quanti) #=> Количество символов в строке
+print(quanti % 2) #=> Остаток от деления на 2
+print(example[6:])
+print(example[::-1])
+print(example[::2])
